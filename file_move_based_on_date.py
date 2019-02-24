c05d16549063b3ee33bc0fae52a54a88a70b04b1
Niehoff80 <34998290+Niehoff80@users.noreply.github.com>
2019-02-24 08:17:15 -0500

    Update file_move_based_on_date.py
    Logfile is created when script is executed.

--- file_move_based_on_date.py
+++ file_move_based_on_date.py
@@ -9,6 +9,7 @@ import os
 import os.path
 import shutil
 import time
+from datetime import datetime
 
 #define
 src = 'C:/test'                                                                             #source folder
@@ -17,16 +18,17 @@ os.chdir(src)                                                                   
 
 #Delete content of logfile
 logfile= open(os.path.join(src, "logfile.txt"),"w")
-logfile.write('Script performed on:' + '\n')
+logfile.write('Script performed on: ' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '\n')
 logfile.close() 
 
 #list through all files in directory
 if os.listdir(src):
     for file in os.listdir('.'):
+        if not file == 'logfile.txt':
             filedate = time.gmtime(os.path.getmtime(file))                                      #get metadata from file
             year = str(filedate.tm_year)                                                        #make string from year
             month = str(filedate.tm_mon)                                                        #make string from month
-
+                        
 #check if year folder already exist if not create one
             folder = os.path.join (dst, year)                                                   #define folder based on year
             if not os.path.isdir(folder):
@@ -52,7 +54,8 @@ if os.listdir(src):
 
 ################################################################ Version ##########################################################################################
 # V1.0 First working version just copying from source to destination folder
-# V1.1 Checks if file already excist in 
+# V1.1 Checks if file already excist in destination
+# V1.2 Create logfile when script is executed
 ################################################################# to do ###########################################################################################
 # 1. check if file already exist in folder before copy or overwrite file -> implemented in V1.1
 # 2. Have this script go through different source folders
